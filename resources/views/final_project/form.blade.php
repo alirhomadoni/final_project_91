0f32461b05605d7bca4a65a8ac4778902eef8e51
Muhammad-Hasan-Syuaibi <muhammadhasansyuaibi@gmail.com>
2020-07-10 23:10:19 +0700

    revisi CKeditor, masih belum bisa tampil

--- resources/views/final_project/form.blade.php
+++ resources/views/final_project/form.blade.php
@@ -2,30 +2,32 @@
 @section('content')
 <div class="ml-3 mt-3 mr-3">
         <div class="card card-primary">
-<<<<<<< HEAD
-            <form role="form" action="/pertanyaan" method="POST">
-=======
+          <form role="form" action="/pertanyaan" method="POST">
             <form role="form" action="/form" method="POST">
->>>>>>> e91845c05ff6630051ceb98c66cf1c259606c7a8
               @csrf
               <label for="">Judul Pertanyaan</label>
               <input class="form-control" type="text" name="judul">
               <label for="">Pertanyaan Baru</label>
-              <textarea  id="isi" class="form-control" name="isi" rows="10" cols="50"></textarea>
+              <textarea id="my-editor" class="form-control" name="isi" rows="10" cols="50"></textarea>
               <input type="hidden" name="user_id" value="{{Auth::user()->id}}">
               <input type="hidden" name="votes" value="0">
               <br>
               <button type="submit" class="btn btn-primary mb-3 ml-3">Create</button> <br>
+              <script>
+                CKEDITOR.replace('my-editor', options);
+              </script>
             </form>
+          </form>
             <a href="{{ url('/pertanyaan') }}">
               <button class="btn btn-outline-primary mb-3 ml-3">Back</button>
             </a>
             <script>
-              var konten = document.getElementById("isi");
-              CKEDITOR.replace(konten,{
-              language:'en-gb'
-              });
-              CKEDITOR.config.allowedContent = true;
-          </script>
+              var options = {
+                filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+                filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+                filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+                filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+              };
+            </script>
           </div>
 @endsection
